eacd957e83a219c119053cdb874bbe607f08b087
Ahmed Hawas <ahmed.hawas@thinkresearchgroup.com>
2016-11-12 20:19:14 -0500

    Creating the topic changeset

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,10 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn, params) do
-    
+    changeset = Topic.changeset(%Topic{}, %{})
+    # empty changeset that is not currently valid
   end
 end
--- web/models/topic.ex
+++ web/models/topic.ex
@@ -1,8 +1,18 @@
 defmodule Discuss.Topic do
   use Discuss.Web, :model
-  
+
   schema "topics" do
     field :title, :string
   end
 
+  def changeset(struct, params \\ %{}) do
+    # struct is the model in the database
+    # params how we are going to update struct
+    # cast produces the changeset
+    struct
+    |> cast(params, [:title])
+    |> validate_required([:title])
+    # output is the final changeset to update and it is
+    # what actually gets into the database
+  end
 end
